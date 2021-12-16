b8f1105e0257f8ff895d0861ca11a4ede29bce57
Kenneth Galbraith <galbrakd@gmail.com>
2021-12-16 00:53:05 -0500

    TicTacToe game start

--- Lesson-5/exercises.rb
+++ Lesson-5/exercises.rb
@@ -232,4 +232,29 @@ arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
 
 arr.select do |hash|
   hash.all? do |key, value|
-    value.each
+    value.all? { |int| int.even? }
+  end
+end
+
+###################################################################################################
+
+# 16.
+# A UUID is a type of identifier often used as a way to uniquely identify items...which may not all be created by the same system.
+# That is, without any form of synchronization, two or more separate computer systems can create new items and label them with a UUID with no significant chance of stepping on each other's toes.
+# It accomplishes this feat through massive randomization. The number of possible UUID values is approximately 3.4 X 10E38.
+# Each UUID consists of 32 hexadecimal characters, and is typically broken into 5 sections like this 8-4-4-4-12 and represented as a string.
+# It looks like this: "f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91"
+# Write a method that returns one UUID when called with no parameters.
+
+def generate_uuid()
+  uuid = ''
+  hyphens = [8, 13, 18, 23]
+  36.times do |i| 
+    if hyphens.include?(i)
+      uuid << '-'
+    else
+      uuid << rand(16).to_s(16)
+    end
+  end
+  uuid
+end
